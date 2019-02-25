96ef901568847772a8fda6ebf315763a418879d2
Hatshepsut <amabelle.delacruz322@gmail.com>
2019-02-24 17:05:46 -0800

    worked on controller. Says burgers.all is not a function. need to finish handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,9 +15,9 @@ router.get("/", function(req, res) {
     res.render("index", hbsObject);
   });
 });
-// ***START AT THIS ROUTER*** //
+
 router.post("/api/burgers", function(req, res) {
-  cat.create([
+  burgers.create([
     "Name", "devoured"
   ], [
     req.body.name, req.body.sleepy
@@ -47,7 +47,7 @@ router.put("/api/burgers/:id", function(req, res) {
 router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  burgers.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
